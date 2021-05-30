54c157074033053b1e47790311a5aadfbcc393c6
Sanjukta Sunam <sanjukta16sunam@gmail.com>
2021-05-30 13:30:30 +0530

    Added Songs
    Added #1

--- songs.sql
+++ songs.sql
@@ -32,3 +32,13 @@ insert into music values(17,"Agar Tum Saath Ho","Tamaasha","Arjit Singh","17.mp3
 insert into music values(18,"Dil De Chuke Sanam","misc","Sanu Kumar","18.mp3","dil_de");
 insert into music values(19,"Nagaadhe sang dhol","Raam Leela","Shreya Goshal","19.mp3","ram");
 insert into music values(20,"Raam Chahe Leela","Raam Leela","Bhoomi Trivedi","20.mp3","ram");
+insert into music values(21,"Atif Aslam Heart Touching song","GIMA Awards","Atif Aslam","21.mp3","gima");
+insert into music values(22,"Bolo Har Har Har","Shivaay","Badshah,Megha Dalton,Mohit Chauhan","22.mp3","shivaay");
+insert into music values(23,"Kabira","Yeh Jawaani Hai Deewani","Harshdeep Kaur,Arijit Singh","23.mp3","kabira");
+insert into music values(24,"Hona Tha Pyaar","Bol","Atif Aslam ,Hadiqa Kiani","24.mp3","bol");
+insert into music values(25,"Rabba Mein Toh Mar Gaya","Mausam","Rahat Fateh Ali Khan","25.mp3","mausam")
+insert into music values(26,"Ishq Shava","Jab Tak Hai Jaan","Raghav Mathur,Shilpa Rao","26.mp3","ishq_shava");
+insert into music values(27,"Yeh Fittor Mera","Fitoor","Arijit Singh","27.mp3","fitoor");
+insert into music values(28,"Jannat Jahan","Jannat","Rupam Islam","28.mp3","jannat");
+insert into music values(29,"Mirzya Title Track","Mirzya","Daler Mehndi,Nooran Sisters,Sain Zahoor,Akhtar Chanal Zahri","@9.mp3","mirzya");
+insert into music values(30,"Oonchi Hai Building","Judwaa 2","Anu Malik","30.mp3","judwaa_2");
